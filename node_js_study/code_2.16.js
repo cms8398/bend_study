f3a69a2e5c969b9021549bc2c0a02d01e41cb112
Minsoo_wsl <cms8398@naver.com>
2025-04-30 20:26:15 +0900

    불필요 파일 삭제

--- node_js_study/code_2.16.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-console.log('String 1');
-const data = fs.readFileSync('./file.txt', 'utf8');
-console.log(data);
-console.log(`Data length: ${data.length} bytes`);
-console.log('String 2');
